d416a1e34fc1efa1e9200761cf68623bbc106fbd
Tom Heyes <tomheyes@Toms-MacBook-Pro.local>
2021-12-06 19:01:32 +0000

    Finish day 5!

--- day5/src/day5/core.clj
+++ day5/src/day5/core.clj
@@ -4,7 +4,7 @@
             [clojure.string :as str]))
 
 (def input
-  (->> "test-input.txt"
+  (->> "input.txt"
        slurp
        str/split-lines
        (mapcat #(str/split %  #" -> "))
@@ -35,20 +35,32 @@
         updated-map
         (recur (inc inital-y) y2 updated-map)))))
 
+(defn draw-diagonal [[[x1 y1] [x2 y2]] sea-map]
+  (let [y-direction (if (< y1 y2) inc dec)]
+    (loop [inital-x x1
+           inital-y y1
+           working-map sea-map]
+      (let [updated-map (update-in working-map [inital-y inital-x] inc)]
+        (if (= inital-x x2)
+          updated-map
+          (recur (inc inital-x) (y-direction inital-y) updated-map))))))
+
 (defn direction [[x1 y1] [x2 y2]]
   (if (= x1 x2)
     (partial draw-vertical x1 (sort [y1 y2]))
-    (partial draw-horizontal y1 (sort [x1 x2]))))
+    (if (= y1 y2)
+      (partial draw-horizontal y1 (sort [x1 x2]))
+      (partial draw-diagonal (sort-by first [[x1 y1] [x2 y2]])))))
 
 (defn mark-map [sea-map cordinates]
   (let [[cord-1 cord-2] (first cordinates)
         draw-function (direction cord-1 cord-2)]
-    (if (seq (rest cordinates))
+    (if (seq cordinates)
       (recur (draw-function sea-map) (rest cordinates))
       sea-map)))
 
 (defn part-1 [input]
-  (let [sea-map (build-map 10 10)
+  (let [sea-map (build-map 1000 1000)
         marked-map (mark-map sea-map input)]
     (doall (map println marked-map))
     (println (->> marked-map
