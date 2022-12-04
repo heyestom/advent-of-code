8511acee0ff2757f5e00ac167b8c9623de4dddd1
Tom Heyes <heyestom@gmail.com>
2022-12-04 10:50:27 +0000

    Advent of code day4 parts 1 and 2 Assignment overlaps

---
+++ 2022/day4/src/day4/core.clj
@@ -0,0 +1,41 @@
+(ns day4.core
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(def input (->> "input.txt"
+                slurp
+                str/split-lines
+                (map #(map (fn [x] (Integer/parseInt x)) (str/split (str/replace % #"[-|,]" " ") #" ")))))
+
+(defn assignments-fully-contained?
+  [[elf-a-start elf-a-end elf-b-start elf-b-end]]
+  (or
+   (and (<= elf-a-start elf-b-start) (>= elf-a-end elf-b-end))
+   (and (<= elf-b-start elf-a-start) (>= elf-b-end elf-a-end) )))
+
+(defn part-1 [assignments]
+  (->> assignments
+       (map assignments-fully-contained?)
+       (filter true?)
+       count))
+
+(defn assignments-overlap?
+  "Is the end of the range with the smallest
+  start coordinate greater than or equal to
+  the start of the range with the largest
+  starting coordinate?"
+  [[elf-a-start elf-a-end elf-b-start elf-b-end]]
+  (if (< elf-a-start elf-b-start)
+      (>= elf-a-end elf-b-start)
+      (>= elf-b-end elf-a-start)))
+
+(defn part-2 [assignments]
+  (->> assignments
+       (map assignments-overlap?)
+       (filter true?)
+       count))
+
+(defn -main
+  [& args]
+  (println "Part 1 - assignments fully contained:" (part-1 input))
+  (println "Part 2 - assignments overlapping:" (part-2 input)))
